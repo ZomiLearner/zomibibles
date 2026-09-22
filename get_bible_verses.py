5875dce1c593f0609650548ff0dd5ef5cbdf6ada
Vum Gualnam <53401737+VumGualnam@users.noreply.github.com>
2026-09-22 08:48:57 +0800

    Refactor Bible verses extraction logic

--- get_bible_verses.py
+++ get_bible_verses.py
@@ -1,37 +1,71 @@
-import sys
-# sys.path.append('/usr/local/lib/python3.13/dist-packages/bible') # Fixing for the broken package (colab)
-sys.path.append('/usr/local/lib/python3.13/site-packages/bible') # Fixing for the broken package (railway)
+# import sys
+# # sys.path.append('/usr/local/lib/python3.13/dist-packages/bible') # Fixing for the broken package (colab)
+# # sys.path.append('/usr/local/lib/python3.13/site-packages/bible') # Fixing for the broken package (railway)
 
-import bible.data
-books = bible.data.bible_data("NKJV") # 31103
-# ('NIV', 'NASB', 'RSV', 'NRSV', 'NCV', 'ESV', 'LB'):
+# import bible.data
+# books = bible.data.bible_data("NKJV") # 31103
+# # ('NIV', 'NASB', 'RSV', 'NRSV', 'NCV', 'ESV', 'LB'):
+
+# book_codes = [
+#     "GEN","EXO","LEV","NUM","DEU","JOS","JDG","RUT",
+#     "1SA","2SA","1KI","2KI","1CH","2CH","EZR","NEH",
+#     "EST","JOB","PSA","PRO","ECC","SNG","ISA","JER",
+#     "LAM","EZK","DAN","HOS","JOL","AMO","OBA","JON",
+#     "MIC","NAM","HAB","ZEP","HAG","ZEC","MAL","MAT",
+#     "MRK","LUK","JHN","ACT","ROM","1CO","2CO","GAL",
+#     "EPH","PHP","COL","1TH","2TH","1TI","2TI","TIT",
+#     "PHM","HEB","JAS","1PE","2PE","1JN","2JN","3JN",
+#     "JUD","REV"
+# ]
+
+# verses = []
+
+# for code, book in zip(book_codes, books):
+#     omissions = book.get("omissions", [])
+
+#     for chapter_num, verse_count in enumerate(book["verse_counts"], start=1):
+
+#         omitted = set()
+#         if chapter_num <= len(omissions) and omissions[chapter_num - 1]:
+#             omitted = set(omissions[chapter_num - 1])
+
+#         for verse_num in range(1, verse_count + 1):
+#             if verse_num not in omitted:
+#                 verse = f"{code}_{chapter_num}_{verse_num}"
+#                 verses.append(verse)
+
+# print(len(verses))
+
+from pythonbible import Book, get_number_of_chapters, get_number_of_verses, get_verse_id
 
 book_codes = [
-    "GEN","EXO","LEV","NUM","DEU","JOS","JDG","RUT",
-    "1SA","2SA","1KI","2KI","1CH","2CH","EZR","NEH",
-    "EST","JOB","PSA","PRO","ECC","SNG","ISA","JER",
-    "LAM","EZK","DAN","HOS","JOL","AMO","OBA","JON",
-    "MIC","NAM","HAB","ZEP","HAG","ZEC","MAL","MAT",
-    "MRK","LUK","JHN","ACT","ROM","1CO","2CO","GAL",
-    "EPH","PHP","COL","1TH","2TH","1TI","2TI","TIT",
-    "PHM","HEB","JAS","1PE","2PE","1JN","2JN","3JN",
-    "JUD","REV"
+    "GEN", "EXO", "LEV", "NUM", "DEU", "JOS", "JDG", "RUT", "1SA", "2SA",
+    "1KI", "2KI", "1CH", "2CH", "EZR", "NEH", "EST", "JOB", "PSA", "PRO",
+    "ECC", "SNG", "ISA", "JER", "LAM", "EZK", "DAN", "HOS", "JOL", "AMO",
+    "OBA", "JON", "MIC", "NAM", "HAB", "ZEP", "HAG", "ZEC", "MAL",
+    "MAT", "MRK", "LUK", "JHN", "ACT", "ROM", "1CO", "2CO", "GAL", "EPH",
+    "PHP", "COL", "1TH", "2TH", "1TI", "2TI", "TIT", "PHM", "HEB", "JAS",
+    "1PE", "2PE", "1JN", "2JN", "3JN", "JUD", "REV"
 ]
 
+# Only take the first 66 books (canonical Protestant Bible)
+books = list(Book)[:66]
+
+# Build verse map + print OSIS references
+index = 1
+verse_map = {}
+
 verses = []
-
-for code, book in zip(book_codes, books):
-    omissions = book.get("omissions", [])
-
-    for chapter_num, verse_count in enumerate(book["verse_counts"], start=1):
-
-        omitted = set()
-        if chapter_num <= len(omissions) and omissions[chapter_num - 1]:
-            omitted = set(omissions[chapter_num - 1])
-
-        for verse_num in range(1, verse_count + 1):
-            if verse_num not in omitted:
-                verse = f"{code}_{chapter_num}_{verse_num}"
-                verses.append(verse)
-
+for book_idx, book in enumerate(books):
+    code = book_codes[book_idx]
+    try:
+        for ch in range(1, get_number_of_chapters(book) + 1):
+            for vs in range(1, get_number_of_verses(book, ch) + 1):
+                vid = get_verse_id(book, ch, vs)
+                verse_map[index] = vid
+                verses.append(f"{code}_{ch}_{vs}")
+                # print(f"{code}_{ch}_{vs}")
+                index += 1
+    except Exception:
+        continue
 print(len(verses))
